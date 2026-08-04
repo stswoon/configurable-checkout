b10f0f64bbbc9229c8e99774b6b7f78d9c9a34a8
stswoon <xx@xx.xx>
2026-08-04 21:27:01 +0400

    add checkout

--- frontend/src/modules/checkout/CheckoutSteps.tsx
+++ frontend/src/modules/checkout/CheckoutSteps.tsx
@@ -1,6 +1,4 @@
 import {useQuote} from "@/hooks/useApi";
-import {useCheckoutContext} from "@/modules/checkout/CheckoutContext";
-import {useState} from "react";
 import {CheckoutConfig} from "@/modules/checkout/types";
 import {CheckoutStep} from "@/modules/checkout/CheckoutStep";
 import {WidgetRenderer} from "@/modules/checkout/WidgetRenderer";
@@ -14,30 +12,6 @@ export interface CheckoutStepsProps {
 export function CheckoutSteps({config, quoteId}: CheckoutStepsProps) {
     const widgetDefinitions = config.widgets;
     const {data: quote} = useQuote(quoteId);
-
-    const {submitStep} = useCheckoutContext();
-
-    const [editStepId, setEditStepId] = useState<string | undefined>(widgetDefinitions?.[0].stepId);
-    const makeStepEdit = (stepId: string) => {
-        setEditStepId(stepId);
-    };
-
-    const goNext = (stepId: string) => {
-        const index = widgetDefinitions.findIndex((w) => w.stepId === stepId);
-        if (index < widgetDefinitions.length - 1) {
-            setEditStepId(widgetDefinitions[index + 1].stepId);
-        } else {
-            setEditStepId(undefined);
-        }
-
-    };
-
-    const handleNext = async (stepId: string) => {
-        const ok = await submitStep(stepId);
-        if (ok) {
-            goNext(stepId);
-        }
-    };
 
     return (
         <div data-test-id="CheckoutSteps">
